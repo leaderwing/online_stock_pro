912834b70561a3f809832495ec24c859bbf66c8d
leaderwing <quyict.hut@gmail.com>
2019-04-01 20:21:09 +0700

    update refresh

--- src/main/resources/static/components/page/xuLyTaiKhoan/xuLyTaiKhoanCtrl.js
+++ src/main/resources/static/components/page/xuLyTaiKhoan/xuLyTaiKhoanCtrl.js
@@ -2,14 +2,26 @@ angular.module('app').controller('xuLyTaiKhoanCtrl',
     ['data', 'modal', '$window', '$rootScope', '$state', '$scope', 'dateFilter', '$interval',
         function (data, modal, $window, $rootScope, $state, $scope, dateFilter, $interval) {
             var vm = this;
+            conn();
+            function conn() {
+                         console.log ("start connect sockjs!");
+                         var socket = new SockJS('/gs-guide-websocket');
+                         stompClient = Stomp.over(socket);
+                         stompClient.connect({}, function (frame) {
+                             console.log('Connected: ' + frame);
+                             stompClient.subscribe('/topic/handleAcc', function (result) {
+                                 var data = JSON.parse(result.body);
+                                 $scope.$apply(function () {
+                                     vm.luongchungse = data.filter(isOwnerData);
+                                 })
+                             });
+                         });
+                         }
             data.luongchungse().then(function (result) {
                             vm.luongchungse = result;
                         }, function (err) {
                             console.log(err);
                         });
-
-//            $interval( function(){
-//                vm.luongchung();}, 1000);
 
             vm.luongchung = function () {
             data.luongchungse().then(function (result) {
@@ -22,10 +34,7 @@ angular.module('app').controller('xuLyTaiKhoanCtrl',
                 $scope.loading = false;
                 $window.localStorage.setItem('idcusid', id);
                 $state.go("root.his-xy-ly-tai-khoan");
-
-
             }
-
             return;
         }
     ])
